8676521cb0f28021f71f4619d4bdd64fbb70a28a
pjroggy <pierrejeanroggy@gmail.com>
2023-05-15 14:25:57 +0200

    quete fini

--- Eagle.java
+++ Eagle.java
@@ -1,4 +1,4 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly{
 
     private boolean flying;
     private int altitude;
@@ -9,6 +9,11 @@ public class Eagle extends Bird {
         this.altitude = 0;
     }
 
+    @Override
+    public String sing() {
+        return "Screech!";
+    }
+
     public int getAltitude() {
         return altitude;
     }
@@ -17,8 +22,68 @@ public class Eagle extends Bird {
         return flying;
     }
 
+    /**
+     * take off, must be on ground
+     */
     @Override
-    public String sing() {
-        return "Screech!";
+    public void takeOff() {
+        if (!this.flying && this.altitude == 0) {
+            this.flying = true;
+            System.out.printf("%s take off into the air.%n", this.getName());
+        }
+    }
+
+    /**
+     * fly upward
+     * 
+     * @param meters altitude increase
+     * @return altitude
+     */
+    @Override
+    public int ascend(int meters) {
+        if (this.flying) {
+            this.altitude = Math.max(this.altitude + meters, 0);
+            System.out.printf("%s flies upward, altitude : %d%n", this.getName(), this.altitude);
+        }
+        return this.altitude;
+    }
+
+    /**
+     * glides, must be into the air
+     */
+    @Override
+    public void glide() {
+        if (this.flying) {
+            System.out.printf("%s glides into the air.%n", this.getName());
+        }
+    }
+
+    /**
+     * fly downward
+     * 
+     * @param meters altitude decrease
+     * @return altitude
+     */
+
+    @Override
+    public int descend(int meters) {
+        if (this.flying) {
+            this.altitude = Math.min(this.altitude - meters, 325);
+            System.out.printf("%s flies downward, altitude : %d%n", this.getName(), this.altitude);
+        }
+        return this.altitude;
+    }
+
+    /**
+     * get landing, must be into the air
+     */
+
+    @Override
+    public void land() {
+        if (this.flying && this.altitude == 0) {
+            System.out.printf("%s lands on the ground.%n", this.getName());
+        } else {
+            System.out.printf("%s is too high, i can't land.%n", this.getName());
+        }
     }
 }
